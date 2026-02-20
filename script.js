db92c966b32c66038512ac4fc3bf660898917278
grdRaphael <drebin235@gmail.com>
2026-02-20 16:18:29 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let listeQuestions = [
+    {
+        question: "Sortir les cheveux mouillés quand il fait froid rend malade",
+        reponse: false,
+        explication: "On tombe malade à cause des virus, pas du froid.",
+        explicationMauvaiseReponse: 'On tombe malade à cause des virus, pas du froid.'
+    },
+    {
+        question: "Craquer ses doigts provoque de l'arthrite",
+        reponse: false,
+        explication: "Aucune étude ne montre de lien avec l'arthrite."
+    },
+    {
+        question: "Il est possible d’attraper un coup de soleil par temps froid",
+        reponse: true,
+        explication: "Les UV sont présents même en hiver."
+    },
+    {
+        question: "La Terre est ronde",
+        reponse: true,
+        explication: "C'est démontré scientifiquement depuis l’Antiquité."
+    }
+]
+
+let zoneQuestion = document.querySelector('.zoneQuestion span')
+let buttonTrue = document.getElementById('buttonTrue')
+let buttonFalse = document.getElementById('buttonFalse')
+let bouttonSuivant = document.querySelector('.bouttonSuivant')
+let i = 0
+
+// Affiche la question suivant apres chaque clique sur le boutton suivant
+function afficherQuestion() {
+    bouttonSuivant.addEventListener('click', () => {
+        i++
+        zoneQuestion.textContent = listeQuestions[i].question
+        bouttonSuivant.classList.add('hidden')
+    })
+}
+
+zoneQuestion.textContent = listeQuestions[i].question
+afficherQuestion()
+bouttonSuivant.classList.add('hidden')
+
+let responseUser = null
+function verifierReponse() {
+    console.log(responseUser)
+}
+
+buttonFalse.addEventListener('click', () => {
+    responseUser = false
+    verifierReponse(responseUser)
+    afficherExplication()
+    bouttonSuivant.classList.remove('hidden')
+    
+})
+
+buttonTrue.addEventListener('click', () => {
+    responseUser = true
+    verifierReponse(responseUser)
+    afficherExplication()
+    bouttonSuivant.classList.remove('hidden')
+})
+
+let explication = document.querySelector(".explication p")
+function afficherExplication() {
+    if (responseUser === listeQuestions[i].reponse) {
+        explication.textContent = `Bonne réponse !
+    ${listeQuestions[i].explication}`
+    } else {
+        explication.textContent = `Mauvaise réponse !
+    ${listeQuestions[i].explication}`
+    }
+}
